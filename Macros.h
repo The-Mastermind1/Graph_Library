3c9fe8f53213652b666807cea0abb738e8d9d01d
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-02-16 22:13:56 +0200

    Create Macros.h

---
+++ Macros.h
@@ -0,0 +1,48 @@
+#pragma once
+#include<exception>
+#define _P_S_BEGIN namespace _panagiotis_stelios{
+
+#define _P_S_END }
+
+#define _NODISCARD [[nodiscard]]
+
+
+struct Term {
+    float coefficient;
+    float exponent;
+
+};
+struct Term2 {
+    float a;
+    float expontential;
+};
+
+struct Term3 {
+    float a;
+    float log;
+};
+
+struct Term4 {
+    float a;
+    float sin;
+};
+struct Term5 {
+    float a;
+    float cos;
+};
+
+class _INVALID_INPUT : public std::exception {
+private:
+    std::string errorMessage; // To store the error message
+public:
+    // Constructor to initialize the error message
+    explicit _INVALID_INPUT(const std::string& message)
+        : errorMessage(message) {
+    }
+
+    // Override the what() method
+    const char* what() const noexcept override {
+        return errorMessage.c_str();
+    }
+};
+
