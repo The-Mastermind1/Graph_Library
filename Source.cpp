da852b4166caad49d36454ab559a5a0c2b422ac2
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-02-16 22:15:12 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,7 @@
 
 
 int main() {
-	using namespace _panagiotis_stelios;
+    using namespace _panagiotis_stelios;
     std::string input = "15x^5+1log(15x)+4cos(55x)"; // Polynomial input string
   
    parse_polynomial(input);
