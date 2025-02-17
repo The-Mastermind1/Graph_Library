b1e5dc277293ee82dd8db55915fc2f44f64164d3
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-02-18 00:27:47 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,18 +2,16 @@
 
 
 int main() {
-   using namespace _panagiotis_stelios;
-
-   std::string input = "15^x"; // Polynomial input string
-   try {
-
-	parse_graph(input);
-   }
-   catch (const _INVALID_INPUT& e) {
-	std::cout << e.what() << '\n';
-   }
- 
+	using namespace _panagiotis_stelios;
 	
-  
+	std::string input = "1x^4"; // Polynomial input string
+	try {
+
+		parse_graph(input);
+	}
+	catch (const _INVALID_INPUT& e) {
+		 std::cout << e.what() << '\n';
+	}
+
    
 }
