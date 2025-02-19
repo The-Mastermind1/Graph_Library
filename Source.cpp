adc70d15508a7958a2d95afbde9c4a627010e082
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-02-19 16:09:04 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,15 +3,14 @@
 
 int main() {
 	using namespace _panagiotis_stelios;
-	
-	std::string input = "1x^4"; // Polynomial input string
-	try {
 
-		parse_graph(input);
-	}
-	catch (const _INVALID_INPUT& e) {
-		 std::cout << e.what() << '\n';
-	}
+std::string input = "5^x+2log(5x)"; // Polynomial input string
+try {
+	parse_graph(input);
+}
+catch (const _INVALID_INPUT& e) {
+	std::cout << e.what() << '\n';
+}
 
    
 }
