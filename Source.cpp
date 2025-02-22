95fa39b371531a988535188e90515850db0c9fff
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-02-22 14:17:13 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,13 +4,20 @@
 int main() {
 	using namespace _panagiotis_stelios;
 
-std::string input = "5^x+2log(5x)"; // Polynomial input string
-try {
-	parse_graph(input);
-}
-catch (const _INVALID_INPUT& e) {
-	std::cout << e.what() << '\n';
-}
+	std::string input = "+5^x+2log(5x)"; // Polynomial input string
+	try {
+		sf::RenderTexture rendertexture;
+		  if (!rendertexture.create(800, 600)) {
+		    throw _FAILED_ACTION{ "Failed to create render texture" };
+		  }
+		
+		  sf::Texture texture;
 
-   
+		parse_graph(input,rendertexture,texture);
+	}
+	catch (const _INVALID_INPUT& e) {
+		std::cout << e.what() << '\n';
+	}
+
+
 }
