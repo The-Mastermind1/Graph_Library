fba89e8210267cf58ab31f383578dee462e546d9
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-02-22 14:14:40 +0200

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,3 +1,4 @@
+
 #pragma once
 #include <iostream>
 #include"Macros.h"
@@ -11,45 +12,51 @@
 
 
 
-
 _P_S_BEGIN
 
 _NODISCARD inline  float evaluate_polyonomial(float cofficient, float  expotent, float x)noexcept {
     std::cout << cofficient << " " << expotent << '\n';
     return static_cast<float>(cofficient * std::pow(x, expotent));
 }
-_NODISCARD inline float evaluate_exponential(float n,float a, float x)noexcept {
+_NODISCARD inline float evaluate_exponential(float n, float a, float x)noexcept {
     std::cout << n << " " << a << '\n';
-    
-    return n*std::exp(a * x);
+
+    return n * std::exp(a * x);
 }
 
 _NODISCARD inline float evaluate_log(float n, float a, float x)noexcept {
     std::cout << n << " " << a << '\n';
-    return n*std::log(a * x);
+    return n * std::log(a * x);
 }
 
 _NODISCARD inline float evaluate_sin(float n, float a, float x)noexcept {
     std::cout << n << " " << a << '\n';
-    return n*std::sin(a * x);
+    return n * std::sin(a * x);
 }
 
 _NODISCARD inline float evaluate_cos(float n, float a, float x)noexcept {
     std::cout << n << " " << a << '\n';
-   
-    return n*std::cos(a * x);
+
+    return n * std::cos(a * x);
 }
-_NODISCARD inline float evaluate_a( float a, float x)noexcept {
+_NODISCARD inline float evaluate_a(float a, float x)noexcept {
     std::cout << a << '\n';
+    if (a > 0) {
+        return std::pow(a, x);
+    }
+    else if (a < 0) {
+        return -std::pow(-a, x);
+    }
 
-    return std::pow(a,x);
+
+    //return std::pow(a, x);
 }
 
 
 
-using Terms = std::variant < Term, Term2, Term3, Term4, Term5,Term6>;
-inline void draw_the_graph(std::vector<Terms>& terms) {
-   
+using Terms = std::variant < Term, Term2, Term3, Term4, Term5, Term6>;
+inline void draw_the_graph(std::vector<Terms>& terms, sf::RenderTexture& rendertexture, sf::Texture& texture) {
+
     /* for (size_t i = 0; i < terms.size(); i++) {
         std::cout << terms[i].coefficient << '\n';
         std::cout << terms[i].exponent << '\n';
@@ -58,7 +65,7 @@ inline void draw_the_graph(std::vector<Terms>& terms) {
     float xmax = 10.0f;
     try {
         float step = 0.1f;
-      
+
         sf::VertexArray graph(sf::LineStrip);//connects the dots on the graph
 
         for (float x = xmin; x <= xmax; x += step) {
@@ -104,7 +111,7 @@ inline void draw_the_graph(std::vector<Terms>& terms) {
 
 
             }
-            std::cout <<"y:" << y << " x: " << x << '\n';
+            std::cout << "y:" << y << " x: " << x << '\n';
             //std::cout << y << '\n';
 
 
@@ -127,7 +134,7 @@ inline void draw_the_graph(std::vector<Terms>& terms) {
         }
 
         sf::VertexArray grid(sf::Lines);
-        
+
         for (int i = 0; i < 800; i += 50) {
             grid.append(sf::Vertex(sf::Vector2f((float)i, 0), sf::Color(200, 200, 200)));
             grid.append(sf::Vertex(sf::Vector2f((float)i, 600), sf::Color(200, 200, 200)));
@@ -166,44 +173,42 @@ inline void draw_the_graph(std::vector<Terms>& terms) {
         //    window.display(); // Display the window
         //}
         //window.~RenderWindow();
-       
-           
-          
-            sf::RenderTexture rendertexture;
-            if (rendertexture.create(800, 600)) {
-
-                rendertexture.clear(sf::Color::Black);
-                rendertexture.draw(grid);
-                rendertexture.draw(axes);
-                rendertexture.draw(graph);
-                rendertexture.display();
-                //sf::sleep(sf::milliseconds(100)); // Small delay to ensure window updates
-
-                sf::Texture texture = rendertexture.getTexture();
 
 
-                sf::Image screenshot = texture.copyToImage();
-                if (!screenshot.saveToFile("output3.png")) {
-                    throw _FAILED_LOADED_IMAGE{ "Failed to load the image\n" };
-                }
-                screenshot.~Image();
-                texture.~Texture();
-               
-            }
-            else {
-                throw _FAILED_ACTION{ "Something went wrong" };
+
+
+        if (rendertexture.create(800, 600)) {
+
+            rendertexture.clear(sf::Color::Black);
+            rendertexture.draw(grid);
+            rendertexture.draw(axes);
+            rendertexture.draw(graph);
+            rendertexture.display();
+            //sf::sleep(sf::milliseconds(100)); // Small delay to ensure window updates
+
+            texture = rendertexture.getTexture();
+
+
+            sf::Image screenshot = texture.copyToImage();
+            if (!screenshot.saveToFile("static/icons/icon.png")) {
+                throw _FAILED_LOADED_IMAGE{ "Failed to load the image\n" };
             }
 
-            //sf::sleep(sf::milliseconds(1000));
-           
-        
-       
+        }
+        else {
+            throw _FAILED_ACTION{ "Something went wrong" };
+        }
+
+        //sf::sleep(sf::milliseconds(1000));
+
+
+
     }
     catch (...) {
         throw _FAILED_ACTION{ "Something went wrong" };
 
     }
-  
+
 
 
     return;
@@ -212,8 +217,8 @@ inline void draw_the_graph(std::vector<Terms>& terms) {
 
 
 
-inline void  parse_graph(const std::string& input) {
-    
+inline void  parse_graph(const std::string& input, sf::RenderTexture& rendertexture, sf::Texture& texture) {
+
     std::vector<Terms> term;
     std::regex pattern1(R"([+-]?([1-9][0-9]*)*x\^[1-9][0-9]*)");
     std::regex pattern2(R"([+-]?([1-9][0-9]*)*e\^[1-9][0-9]*x)");
@@ -222,8 +227,8 @@ inline void  parse_graph(const std::string& input) {
     std::regex pattern5(R"([+-]?([1-9][0-9]*)*cos\([1-9][0-9]*x\))");
     std::regex pattern6(R"([+-]?[1-9][0-9]*\^x)");
 
-   
-   
+
+
     std::sregex_iterator begin1(input.cbegin(), input.cend(), pattern1);
     std::sregex_iterator begin2(input.cbegin(), input.cend(), pattern2);
     std::sregex_iterator begin3(input.cbegin(), input.cend(), pattern3);
@@ -234,97 +239,107 @@ inline void  parse_graph(const std::string& input) {
     std::smatch match;
     std::string kati;
 
-   
+
     std::string::iterator it2;
 
     std::size_t iterations = 0;//see the iterations
-    iterations+=std::distance(begin1, end);
+    iterations += std::distance(begin1, end);
     iterations += std::distance(begin2, end);
     iterations += std::distance(begin3, end);
     iterations += std::distance(begin4, end);
     iterations += std::distance(begin5, end);
     iterations += std::distance(begin6, end);
-   
+
     term.reserve(iterations);
 
 
     for (std::sregex_iterator it = begin1; it != end; ++it) {
-        
-       
-         match = *it;
 
-         
-         kati= match.str() ;//ax^n
 
-         it2 = std::find(kati.begin(), kati.end(),'^');
-         std::string kati2{ it2 + 1,kati.end() };
+        match = *it;
 
-       
-      
-           if (kati[0] != 'x') {
-               try {
-                   term.emplace_back<Term>({ std::stof(kati),std::stof(kati2) });
-               }
-               catch (...) {
 
-                   throw _INVALID_INPUT{ "Invalid Input" };
-               }
-               
-           }
-           else {
-               term.emplace_back<Term>({1,std::stof(kati2)});
-           }
-       
-       
+        kati = match.str();//ax^n
 
-       
+        it2 = std::find(kati.begin(), kati.end(), '^');
+        std::string kati2{ it2 + 1,kati.end() };
+
+
+
+        if (kati[0] != 'x') {
+            try {
+                term.emplace_back<Term>({ std::stof(kati),std::stof(kati2) });
+            }
+            catch (...) {
+
+                throw _INVALID_INPUT{ "Invalid Input" };
+            }
+
+        }
+        else {
+            try {
+                term.emplace_back<Term>({ 1,std::stof(kati2) });
+            }
+            catch (...) {
+                throw _INVALID_INPUT{ "Invalid Input" };
+            }
+        }
+
+
+
+
 
     }
-  
-   
+
+
 
     for (std::sregex_iterator it = begin2; it != end; ++it) {
-        
-       match = *it;  
-       kati= match.str();//e^ax
-       
-        
-       it2 = std::find(kati.begin(), kati.end(), '^');
-       std::string kati2{ it2 + 1,kati.end() };
-      
-      
-          
-       
-       if (kati[0] != 'e') {
+
+        match = *it;
+        kati = match.str();//e^ax
+
+
+        it2 = std::find(kati.begin(), kati.end(), '^');
+        std::string kati2{ it2 + 1,kati.end() };
+
+
+
+
+        if (kati[0] != 'e') {
             try {
                 term.emplace_back<Term2>({ std::stof(kati),std::stof(kati2) });
             }
             catch (...) {
                 throw _INVALID_INPUT{ "Invalid Input" };
             }
-       }
-       else {
-            term.emplace_back<Term2>({ 1,std::stof(kati2) });
-       }
-        
-       
-      
-        
+        }
+        else {
+            try {
+                term.emplace_back<Term2>({ 1,std::stof(kati2) });
+            }
+            catch (...) {
+                throw _INVALID_INPUT{ "Invalid Input" };
+            }
+        }
+
+
+
+
 
 
     }
     for (std::sregex_iterator it = begin3; it != end; ++it) {
-        
+
         match = *it;
-       
-        kati= match.str() ;//nlog(ax)
-      
-       
+
+        kati = match.str();//nlog(ax)
+
+
         it2 = std::find(kati.begin(), kati.end(), '(');
         std::string kati2{ it2 + 1,kati.end() };
-      
-       
-        
+
+
+
         if (kati[0] != 'l') {
             try {
                 term.emplace_back<Term3>({ std::stof(kati),std::stof(kati2) });
@@ -334,21 +349,26 @@ inline void  parse_graph(const std::string& input) {
             }
         }
         else {
-            term.emplace_back<Term3>({ 1,std::stof(kati2) });
+            try {
+                term.emplace_back<Term3>({ 1,std::stof(kati2) });
+            }
+            catch (...) {
+                throw _INVALID_INPUT{ "Invalid Input" };
+            }
         }
 
-       
+
     }
     for (std::sregex_iterator it = begin4; it != end; it++) {
-        
+
         match = *it;
-        kati= match.str() ;//nsin(ax)
-      
+        kati = match.str();//nsin(ax)
+
         it2 = std::find(kati.begin(), kati.end(), '(');
         std::string kati2{ it2 + 1,kati.end() };
-       
-      
-        
+
+
+
         if (kati[0] != 's') {
             try {
                 term.emplace_back<Term4>({ std::stof(kati),std::stof(kati2) });
@@ -360,61 +380,71 @@ inline void  parse_graph(const std::string& input) {
 
         }
         else {
-            term.emplace_back<Term4>({1,std::stof(kati2)});
+            try {
+                term.emplace_back<Term4>({ 1,std::stof(kati2) });
+            }
+            catch (...) {
+                throw _INVALID_INPUT{ "Invalid Input" };
+            }
         }
 
     }
     for (std::sregex_iterator it = begin5; it != end; it++) {
         match = *it;
-        kati= match.str();//ncos(ax)
-        
+        kati = match.str();//ncos(ax)
+
         it2 = std::find(kati.begin(), kati.end(), '(');
         std::string kati2{ it2 + 1,kati.end() };
-       
-       
-        
-       if(kati[0]!='c') {
-           try {
-               term.emplace_back<Term5>({std::stof(kati),std::stof(kati2)});
-           }
-           catch (...) {
-               throw _INVALID_INPUT{ "Invalid Input" };
 
-           }
-       }
-       else {
-           term.emplace_back<Term5>({1,std::stof(kati2)});
-       }
 
-       
+
+        if (kati[0] != 'c') {
+            try {
+                term.emplace_back<Term5>({ std::stof(kati),std::stof(kati2) });
+            }
+            catch (...) {
+                throw _INVALID_INPUT{ "Invalid Input" };
+
+            }
+        }
+        else {
+            try {
+                term.emplace_back<Term5>({ 1,std::stof(kati2) });
+            }
+            catch (...) {
+                throw _INVALID_INPUT{ "Invalid Input" };
+            }
+        }
+
+
     }
-    
+
     for (std::sregex_iterator it = begin6; it != end; it++) {
-         match = *it;
-         kati= match.str() ;//ncos(ax)
-         
-       
-         term.emplace_back<Term6>({std::stof(kati)});
+        match = *it;
+        kati = match.str();//a^x
+        try {
+
+            term.emplace_back<Term6>({ std::stof(kati) });
+        }
+        catch (...) {
+            throw _INVALID_INPUT{ "Invalid Input" };
+        }
+
 
 
     }
 
 
-   
-    
-   
+
+
+
     if (term.size() == 0) {
         throw _INVALID_INPUT{ "Invalid Input" };
     }
-    draw_the_graph(term);
+    draw_the_graph(term, rendertexture, texture);
     return;
 
 }
 
 _P_S_END
-
-
-
-
-
 
